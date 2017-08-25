4091c0e1c359e0d38b0084a583a91d07952f91a1
Mauricio Junior <mauricio.junior@gmail.com>
2017-08-25 14:54:29 -0300

    criando framework
    criando framework
    consumindo framework

---
+++ WindowsDesktopConsumindoFramework/WindowsDesktopConsumindoFramework/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsDesktopConsumindoFramework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblAno = new System.Windows.Forms.Label();
+            this.txtAno = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(518, 39);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(191, 79);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblAno
+            // 
+            this.lblAno.AutoSize = true;
+            this.lblAno.Location = new System.Drawing.Point(40, 63);
+            this.lblAno.Name = "lblAno";
+            this.lblAno.Size = new System.Drawing.Size(175, 32);
+            this.lblAno.TabIndex = 1;
+            this.lblAno.Text = "Digite o ano:";
+            // 
+            // txtAno
+            // 
+            this.txtAno.Location = new System.Drawing.Point(230, 60);
+            this.txtAno.Name = "txtAno";
+            this.txtAno.Size = new System.Drawing.Size(264, 38);
+            this.txtAno.TabIndex = 2;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(76, 142);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 32);
+            this.lblResultado.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 518);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtAno);
+            this.Controls.Add(this.lblAno);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblAno;
+        private System.Windows.Forms.TextBox txtAno;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
